6a2f937afd8c25d5d3a2819bf071c23499d4c1c9
sawpyae <sawpyae.htun@haulio.io>
2019-08-23 12:21:26 +0630

    Initial Commit

---
+++ ListDetailsAppWithTableViewAndScrollVeiw/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ListDetailsAppWithTableViewAndScrollVeiw
+//
+//  Created by saw pyaehtun on 23/08/2019.
+//  Copyright © 2019 saw pyaehtun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
